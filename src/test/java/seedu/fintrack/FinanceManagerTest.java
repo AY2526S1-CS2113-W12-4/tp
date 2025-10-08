48852a8142c55c27325f241fab82bfb8da252fed
duckyfuz <108561447+duckyfuz@users.noreply.github.com>
2025-10-08 14:30:59 +0800

    Resolve linting errors

--- src/test/java/seedu/fintrack/FinanceManagerTest.java
+++ src/test/java/seedu/fintrack/FinanceManagerTest.java
@@ -3,7 +3,6 @@ package seedu.fintrack;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.time.LocalDate;
 import java.util.List;
